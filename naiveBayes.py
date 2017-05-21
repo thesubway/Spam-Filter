d81c168e573cdc065434b4b8813a85c89fc59df8
Alma Niu <aniu@scu.edu>
2017-05-21 16:50:11 -0700

    cleaning words

--- naiveBayes.py
+++ naiveBayes.py
@@ -1,22 +1,28 @@
-print ('Hello')
-ham1 = []
-spam2 = []
+import re
+ham1= []
+spam2 =[]
 count = 0;
-with open('file1.py', 'r') as t:
+with open('file1.py','r') as t:
     for a in t:
-        if a[0] == 'h':
-            ham1.append(a)
-        elif a[0] == 's':
-            spam2.append(a)
+    if a[0]=='h':
+        ham1.append(a)
+    elif a[0]=='s':
+        spam2.append(a)
 
 
-track =0;
-for j in ham1:  # to isolate each words in j
-    for s in j:
-        print(s)
-    track+=1
-    if track==1:
-        break
+words=list()
+s =0
+for j in ham1:
+    #print(j)
+    tester=j
+        s+=1
+            if s==1:
+break
+tester=tester[3:]
+print(tester)
+cleanString=re.sub('\W+', ' ',tester)
+print(cleanString.lower()) #this remove all commas, periods, #,/ and make words in lower case
+
 
 """
 for t in spam2:
@@ -28,6 +34,11 @@ print('ay')
 
 """
 Git Lessons:
+To get to Spam Folder:
+cd Documents
+cd "___" //drage the folter
+
+
 cd /Users/Alma/Downloads/pam-Filter-Master
 Spam-Filter-master Alma$ git status
 
@@ -38,7 +49,7 @@ git clone https://github.com/thesubway/Spam-Filter.git   /*prob dragged*/
 cd Spam-Filter
 git status
 git add .
-git commit
+
 git commit -m "test"
 git push
 
