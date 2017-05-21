7beb6db6030ebc57ca6c495fd77237b7f42ec3e3
Alma Niu <aniu@scu.edu>
2017-05-21 16:22:33 -0700

    anotherchange

--- naiveBayes.py
+++ naiveBayes.py
@@ -9,9 +9,40 @@ with open('file1.py', 'r') as t:
         elif a[0] == 's':
             spam2.append(a)
 
-for j in ham1:  #
-    print(j)
 
+track =0;
+for j in ham1:  # to isolate each words in j
+    for s in j:
+        print(s)
+    track+=1
+    if track==1:
+        break
+
+"""
 for t in spam2:
     print(t)
 print('ay')
+"""
+
+
+
+"""
+Git Lessons:
+cd /Users/Alma/Downloads/pam-Filter-Master
+Spam-Filter-master Alma$ git status
+
+cd /Users/Alma/Downloads/Spam---something master
+git status
+cd ..
+git clone https://github.com/thesubway/Spam-Filter.git   /*prob dragged*/
+cd Spam-Filter
+git status
+git add .
+git commit
+git commit -m "test"
+git push
+
+
+
+
+"""
